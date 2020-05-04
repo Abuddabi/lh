c774e0133f8d91685e3de7ca360c1446640db430
Abuddabi <web.abudabi@gmail.com>
2020-05-04 16:09:05 +0300

    Added player positions

--- src/Entity/Player.php
+++ src/Entity/Player.php
@@ -8,6 +8,7 @@ class Player
 
     private int $number;
     private string $name;
+    private string $position;
     private string $playStatus;
     private int $goals;
     private int $yellowCards;
@@ -15,10 +16,11 @@ class Player
     private int $inMinute;
     private int $outMinute;
 
-    public function __construct(int $number, string $name)
+    public function __construct(int $number, string $name, string $position)
     {
         $this->number = $number;
         $this->name = $name;
+        $this->position = $position;
         $this->playStatus = self::BENCH_PLAY_STATUS;
         $this->goals = 0;
         $this->yellowCards = 0;
@@ -35,6 +37,11 @@ class Player
     public function getName(): string
     {
         return $this->name;
+    }
+
+    public function getPosition(): string
+    {
+        return $this->position;
     }
 
     public function getGoals(): int
